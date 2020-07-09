a0374cd6426a2c8f21ab507e3fa86d27ba16df11
Ricardo Chavez <rachav999iron@gmail.com>
2020-07-09 18:23:29 -0500

    Pregunta 02 - Listo

---
+++ Preg02.py
@@ -0,0 +1,7 @@
+# Autor: Chavez Vilcapoma Ricardo Alfonso
+
+texto = input('Escriba el texto: ')
+if('@untels.edu.pe' in texto):
+  print('Si es correo de untels')
+else:
+  print('No es correo de untels')
