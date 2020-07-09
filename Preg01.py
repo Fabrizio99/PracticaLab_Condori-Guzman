d85fb05ea38d79de17c92dd999a00931952521e9
alvaro <royerdcq@gmail.com>
2020-07-09 18:04:03 -0500

    De la Cruz Quispe Royer Alvaro 01

---
+++ Preg01.py
@@ -0,0 +1,24 @@
+
+
+def esPrimo(num):
+    if num < 1:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True            
+
+def applicacion():
+    numero = int(input('escribe un numero: '))
+    resultado = esPrimo(numero)
+
+    if resultado is True:
+        print('el numero es primo!!')
+    else:
+        print('el numero no es primo!!')
+
+
+applicacion()
